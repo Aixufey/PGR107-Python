70181115851c5361fa1a53dff483d57b9239048a
Aixufey <jack_11728@msn.com>
2023-01-27 19:41:49 +0100

    Cleaned up branch

--- 3_Decision/task/middlechar.py
+++ 3_Decision/task/middlechar.py
@@ -12,24 +12,17 @@ Created on Fri Jan 27 13:50:02 2023
 
 usrinput = input("Enter a string: ")
 
+# Position is the index of (length of a string floor division by 2)
+# Position - 1 is the lowerbound char + Position is the upperbound char
 position = len(usrinput) // 2
 
 
 if len(usrinput) % 2 == 1:
-    print("odd")
+    # odd number gives the middle char
     result = usrinput[position]
     
 else:
-    print("even")
+    # if even number, position-1 gives the left char and position gives the right char
     result = usrinput[position-1] + usrinput[position] 
     
 print("Middle character(s) in '%s' is '%s' " % (usrinput, result))
-
-# if int(len(usrinput)) % 2 == 1:
-#     # odd number gives the middle char
-#     result = usrinput[position]
-# else:
-#     # if even number, position-1 gives the left char and position gives the right char
-#     result = usrinput[position-1] + usrinput[position]
-
-# print("Middle character(s) in '%s' is '%s' " % (usrinput, result))
